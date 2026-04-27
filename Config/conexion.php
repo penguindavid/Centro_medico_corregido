fd05b1ae3e515c76ee88c335aa9ad172a8d8fd1d
penguindavid <davidmacias@colegioguanenta.edu.co>
2026-04-27 15:44:43 -0500

    jesus

---
+++ Config/conexion.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Centro medico Jesus Jose y Maria</title>
+</head>
+<body>
+    <h1>Centro medico Jesus Jose y Maria</h1>
+    <H2>David Santiago Macias Maldonado</H2>
+</body>
+</html>
